35d59c9c92be34d21a294f67ae01b02ce3a0a1a1
deepak <deepaky@hyperlinkinfosystem.net.in>
2023-11-05 21:31:59 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Fitness Application(health is wealth)
+
+![React Fitness Application](https://i.ibb.co/Yt9spGc/image.png)
+
+
---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '8ebe66c1b4msh326d58e8128beabp1fe8b9jsna829fbca6af5',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '8ebe66c1b4msh326d58e8128beabp1fe8b9jsna829fbca6af5',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
